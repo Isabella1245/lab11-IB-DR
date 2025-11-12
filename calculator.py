8d7412177c6500380f1afb98a00f554915f9d9f6
Isabella Bodea <99446329+Isabella1245@users.noreply.github.com>
2025-11-12 08:30:09 -0500

    revert changes

--- calculator.py
+++ calculator.py
@@ -1,28 +1,9 @@
-import math
+"""
+calculator.py
+- Defines functions used to create a simple calculator
 
-def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return "Error: Division by zero is not allowed."
-    
-def log(a, b):
-    try:
-        return math.log(a, b)
-    except ValueError:
-        return "Error: Invalid input for logarithm."
-    except ZeroDivisionError:
-        return "Error: Logarithm base cannot be zero."
-    
-def exp(a, b):
-    return a ** b
-
+One function per operation, in order.
+"""
+# First example
+def add(a, b): 
+    pass
